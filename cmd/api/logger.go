e1e4597249ae902853254e725e91b87b3a94ce9e
Jackie <kennedy.lsd32@gmail.com>
2025-02-15 23:41:02 +0100

    fix logger bug

--- cmd/api/logger.go
+++ cmd/api/logger.go
@@ -3,11 +3,13 @@ package api
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s", r.Method, r.RequestURI)
+		start := time.Now()
 		next.ServeHTTP(w, r)
+		log.Printf("%s %s takes %d ms", r.Method, r.RequestURI, time.Since(start).Milliseconds())
 	})
 }
